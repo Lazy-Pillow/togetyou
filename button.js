83eae21a520b4533cf149345dd2318c5a3008674
Lazy-Pillow <159009939+Lazy-Pillow@users.noreply.github.com>
2024-02-09 17:22:08 +0530

    Delete button.js

--- button.js
+++
@@ -1,8 +0,0 @@
-var button = document.querySelector("button");
-
-button.addEventListener("click", function() {
-  var changeTop = (Math.random() * ($(window).height() - $("button").height()));
-  var changeLeft = (Math.random() * ($(window).width() - $("button").width()));
-  $("button").css("margin-top", changeTop + "px");
-  $("button").css("margin-left", changeLeft + "px");
-});
